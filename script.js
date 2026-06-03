fd49cae5669b171a760f9968b106826775c6d971
ali ahmadi <aliahmadi1999001378@gmail.com>
2026-06-03 22:50:33 +0330

    Add a conditional statement

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ document.querySelector('.check').addEventListener('click',function(){
     const guess = Number( document.querySelector('.guess').value);
     if (!guess) {
         document.querySelector('.message').textContent = ' 😒 عددی وجود ندارد'
+    }else if(guess === secretNumber){
+       document.querySelector('.message').textContent = "درست حدس زدی ایول به تو  🤗🤗"
+    }else if(guess > secretNumber){
+      document.querySelector('.message').textContent = "عدد خیلی بالا زدی  بیا پایین تر 🔺 "
+    }else if(guess < secretNumber){
+      document.querySelector('.message').textContent = "عدد خیلی پایین  زدی  بیا بالا تر 🔻 "
     }
 });
 // Check Btn End
