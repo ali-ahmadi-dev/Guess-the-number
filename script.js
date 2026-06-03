c03f334f8a9774b49793a2f67a88d2d29b9acbbe
ali ahmadi <aliahmadi1999001378@gmail.com>
2026-06-03 21:30:06 +0330

    Check Btn  addEventListener

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+
+
+// Check Btn
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number( document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = ' 😒 عددی وجود ندارد'
+    }
+})
