1bfee16f03fbf17f1f02f384e092fde21f9e910e
ali ahmadi <aliahmadi1999001378@gmail.com>
2026-06-04 18:49:41 +0330

    Add game logic

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 
 
 // Method math
-const secretNumber =Math.trunc(Math.random() * 20 ) + 1;
+const secretNumber = Math.trunc(Math.random() * 20 ) + 1;
 //  Method math End
 
+
+let score = 20;
 
 
 // Start  Check Btn
@@ -16,9 +18,29 @@ document.querySelector('.check').addEventListener('click',function(){
     }else if(guess === secretNumber){
        document.querySelector('.message').textContent = "درست حدس زدی ایول به تو  🤗🤗"
     }else if(guess > secretNumber){
+      if (score > 1) {
       document.querySelector('.message').textContent = "عدد خیلی بالا زدی  بیا پایین تر 🔺 "
+       score--;
+        document.querySelector('.score').textContent = score;
+        }else{
+             score = 0;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.message').textContent = "شما بازی را باختید 😔";
+        }
+
     }else if(guess < secretNumber){
-      document.querySelector('.message').textContent = "عدد خیلی پایین  زدی  بیا بالا تر 🔻 "
+
+      if (score > 1 ) {
+       document.querySelector('.message').textContent = "عدد خیلی پایین  زدی  بیا بالا تر 🔻 "
+      score--;
+      document.querySelector('.score').textContent = score;
+
+      }else{
+       score = 0;
+       document.querySelector('.score').textContent = score;
+       document.querySelector('.message').textContent = "شما بازی را باختید 😔";
+      }
+
     }
 });
 // Check Btn End
